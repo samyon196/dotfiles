feb854e93e350d5932784870c1a1c3186fb18f8a
samyon196 <samyon.v@gmail.com>
2020-10-08 19:45:50 +0300

    fix: minor p10k fix

--- software/install.sh
+++ software/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # cathup on repos
 sudo pacman -Sy
+
 # Install from official repos
 sudo pacman -S --noconfirm base-devel binutils git yay zsh tmux vim z bat fd ripgrep exa ttf-nerd-fonts-symbols
-# Add z to work..
 
+# Add z to work..
 echo "[[ -r \"/usr/share/z/z.sh\" ]] && source /usr/share/z/z.sh" >> ~/.zshrc
 # Install from AUR
 yay -S --noconfirm peco-bin icdiff dust-bin ruby-colorls
@@ -12,8 +13,8 @@ yay -S --noconfirm peco-bin icdiff dust-bin ruby-colorls
 # install tldr
 curl -o /usr/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
 chmod +x /usr/bin/tldr
-echo "TLDR_HEADER='magenta bold underline'" >> ~/.zshrc
-echo "TLDR_QUOTE='italic'" >> ~/.zshrc
-echo "TLDR_DESCRIPTION='green'" >> ~/.zshrc
-echo "TLDR_CODE='yellow'" >> ~/.zshrc
-echo "TLDR_PARAM='red'" >> ~/.zshrc
+echo "export TLDR_HEADER='magenta bold underline'" >> ~/.zshrc
+echo "export TLDR_QUOTE='italic'" >> ~/.zshrc
+echo "export TLDR_DESCRIPTION='green'" >> ~/.zshrc
+echo "export TLDR_CODE='yellow'" >> ~/.zshrc
+echo "export TLDR_PARAM='red'" >> ~/.zshrc
