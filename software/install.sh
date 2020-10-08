5fba2ab61f9b0a108b92a8becebc3be879ede798
samyon196 <samyon.v@gmail.com>
2020-10-08 19:28:15 +0300

    feat: added tldr works fine

--- software/install.sh
+++ software/install.sh
@@ -3,5 +3,17 @@
 sudo pacman -Sy
 # Install from official repos
 sudo pacman -S --noconfirm base-devel binutils git yay zsh tmux vim z bat fd ripgrep exa ttf-nerd-fonts-symbols
+# Add z to work..
+
+echo "[[ -r \"/usr/share/z/z.sh\" ]] && source /usr/share/z/z.sh" >> ~/.zshrc
 # Install from AUR
-yay -S --noconfirm tealdeer-bin peco-bin icdiff dust ruby-colorls
+yay -S --noconfirm peco-bin icdiff dust-bin ruby-colorls
+
+# install tldr
+curl -o /usr/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
+chmod +x /usr/bin/tldr
+echo "TLDR_HEADER='magenta bold underline'" >> ~/.zshrc
+echo "TLDR_QUOTE='italic'" >> ~/.zshrc
+echo "TLDR_DESCRIPTION='green'" >> ~/.zshrc
+echo "TLDR_CODE='yellow'" >> ~/.zshrc
+echo "TLDR_PARAM='red'" >> ~/.zshrc
