fd1b8dd258df2d6c2191af9d43da37956c676993
samyon196 <samyon.v@gmail.com>
2020-10-08 20:01:48 +0300

    fix: chsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,3 +2,6 @@
 #Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+
+sudo chsh /bin/zsh manjaro # TODO!! 
