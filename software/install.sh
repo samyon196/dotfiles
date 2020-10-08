e4332668df89748265328f5eb4d7da853aaf63a5
samyon196 <samyon.v@gmail.com>
2020-10-08 18:22:19 +0300

    feat: added aur

--- software/install.sh
+++ software/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# cathup on repos
+sudo pacman -Sy
 # Install from official repos
-sudo pacman -S --noconfirm git yay zsh tmux vim z bat fd ripgrep exa ttf-nerd-fonts-symbols
+sudo pacman -S --noconfirm base-devel binutils git yay zsh tmux vim z bat fd ripgrep exa ttf-nerd-fonts-symbols
 # Install from AUR
-sudo yay -S --noconfirm tealdeer-bin peco-bin icdiff dust ruby-colorls
+yay -S --noconfirm tealdeer-bin peco-bin icdiff dust ruby-colorls
