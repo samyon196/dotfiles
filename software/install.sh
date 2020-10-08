095f11fd094564cfbdbc564cf018f63a613be09f
samyon196 <samyon.v@gmail.com>
2020-10-08 17:21:21 +0300

    feat: added software installation scripts

---
+++ software/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Install from official repos
+sudo pacman -S --noconfirm git yay zsh tmux vim z bat fd ripgrep exa ttf-nerd-fonts-symbols
+# Install from AUR
+sudo yay -S --noconfirm tealdeer-bin peco-bin icdiff dust ruby-colorls
